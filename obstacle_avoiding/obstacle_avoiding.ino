552d9e63193f1f5e583434432d8d1fdd48bf8f39
Daniel <danielrobinson095@gmail.com>
2017-09-08 21:33:43 -0400

    Adding project file

---
+++ obstacle_avoiding/obstacle_avoiding.ino
@@ -0,0 +1,20 @@
+const int PIN_MOTOR_A1 = 6;
+const int PIN_MOTOR_A2 = 7;
+const int PIN_MOTOR_B1 = 8;
+const int PIN_MOTOR_B2 = 9;
+const int US_TRIGGER = 10;
+const int US_ECHO = 11;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
